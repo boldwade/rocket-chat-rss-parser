a298e408b638752b19971dc3c51d00ba49b9a063
Wade Walker <wadeswalker@gmail.com>
2019-01-14 11:10:15 -0600

    Init Commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+This project simply take a collection of rss feeds, parses the top 10 (via rss-parser) then issues post requests to an incoming webhook that you have setup in RocketChat.
+// export NODE_TLS_REJECT_UNAUTHORIZED="0" && node rss-parser-rocket-chat.js
---
+++ rss-parser-rocket-chat.js
@@ -0,0 +1,64 @@
+const Parser = require('rss-parser');
+const request = require("request");
+const rssFeeds = require('../rss-rocket-chat/rss-feeders.json');
+const maxNumberOfPostsPerRssFeed = 10;
+
+const postToRocketChat = async(rocketChatPosts, webhookUrl) => {
+    const options = {
+        method: 'POST',
+        url: webhookUrl
+    };
+
+    rocketChatPosts.forEach(body => {
+        options.body = JSON.stringify(body);
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log('ERROR', error);
+                throw new Error(error);
+            } else {
+                if (response.statusCode !== 200) {
+                    console.log('ERROR', response.statusCode, response.statusMessage);
+                    throw new Error(response.statusMessage);
+                }
+            }
+        });
+    });
+}
+
+const getRssFeeds = async() => {
+    const parser = new Parser();
+
+    try {
+        await rssFeeds
+            .feeds
+            .forEach(async(feed) => {
+
+                await feed
+                    .urlFeeds
+                    .forEach(async(url) => {
+                        const rocketChatPosts = [];
+                        const rssData = await parser.parseURL(url);
+
+                        rssData
+                            .items
+                            .slice(0, maxNumberOfPostsPerRssFeed)
+                            .forEach(item => {
+                                const rssObj = {
+                                    color: "#764FA5",
+                                    icon_emoji: ":your_icon_here:",
+                                    text: item.link
+                                };
+
+                                rocketChatPosts.push(rssObj);
+                            });
+
+                        postToRocketChat(rocketChatPosts, feed.webhookUrl);
+                    });
+            });
+
+    } catch (error) {
+        console.log('Error occurred', error);
+    }
+}
+
+getRssFeeds();
